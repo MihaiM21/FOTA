fa018f01efdb9253d4e52515961c4ef2d88ee8ac
MihaiM21 <mihai.marinescu2112@yahoo.com>
2025-02-13 17:30:13 +0200

    Added driver list for every year

---
+++ FOTA/driversList.cs
@@ -0,0 +1,52 @@
+﻿namespace FOTA;
+
+public class driversList
+{
+    public readonly Dictionary<int, string> drivers2025 = new Dictionary<int, string>
+    {
+        { 1, "HAM" },
+        { 2, "LEC" },
+        { 3, "NOR" },
+        { 4, "PIA" },
+        { 5, "RUS" },
+        { 6, "ANT" },
+        { 7, "VER" },
+        { 8, "LAW" },
+        { 9, "ALO" },
+        { 10, "STR" },
+        { 11, "GAS" },
+        { 12, "DOO" },
+        { 13, "OCO" },
+        { 14, "BEA" },
+        { 15, "ALB" },
+        { 16, "SAI" },
+        { 17, "HUL" },
+        { 18, "BOR" },
+        { 19, "TSU" },
+        { 20, "HAD" }
+    };
+    
+    public readonly Dictionary<int, string> drivers2024 = new Dictionary<int, string>
+    {
+        { 1, "HAM" },
+        { 2, "RUS" },
+        { 3, "LEC" },
+        { 4, "SAI" },
+        { 5, "NOR" },
+        { 6, "PIA" },
+        { 7, "VER" },
+        { 8, "PER" },
+        { 9, "ALO" },
+        { 10, "STR" },
+        { 11, "GAS" },
+        { 12, "OCO" },
+        { 13, "MAG" },
+        { 14, "HUL" },
+        { 15, "ALB" },
+        { 16, "COL" },
+        { 17, "BOT" },
+        { 18, "ZHO" },
+        { 19, "TSU" },
+        { 20, "LAW" }
+    };
+}
